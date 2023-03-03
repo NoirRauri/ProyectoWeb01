928d20bd92d3ea5d4b700d4ee52f136c0c38710c
Michael Alvarez <lauryyhorel@gmail.com>
2023-03-03 17:26:28 -0600

    jsbtnAccount-resgister

--- Razer/JS/main.js
+++ Razer/JS/main.js
@@ -19,3 +19,26 @@ window.addEventListener("scroll", function () {
   btn.style.marginTop = value * 1.5 + "px";
 });
 // fin de la animacion
+
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".loginLink");
+const registerLink = document.querySelector(".registerLink");
+
+const btnPopup = document.querySelector(".btnLogin-popup");
+const iconClose = document.querySelector(".iconClose");
+
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("active");
+});
+
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+});
+
+btnPopup.addEventListener("click", () => {
+  wrapper.classList.add("active-popup");
+});
+
+iconClose.addEventListener("click", () => {
+  wrapper.classList.remove("active-popup");
+});
